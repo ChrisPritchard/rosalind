449e4697160f1c2c5cc97e3f9203eb313b29d9aa
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-07-30 14:39:19 +1200

    finished 12 with a fix

--- 012_mortal-fibonacci-rabbits.fsx
+++ 012_mortal-fibonacci-rabbits.fsx
@@ -11,17 +11,17 @@ By 1950, Australian rabbits numbered 600 million, causing the government to deci
 The bunnies will not be stopped, but they don't live forever, and so in this problem, our aim is to expand Fibonacci's rabbit population model to allow for mortal rabbits.
 *)
 
+// n = months, m = life per rabbit pair (including one month prior to breeding age)
 let n, m = 98, 16
-//let n, m = 6, 3 // months, life
 
 let alive = Array.create (n + 1) (bigint 0)
 let deaths = Array.create (n + m + 2) (bigint 0)
 
-alive.[2] <- bigint 2
-deaths.[m + 1] <- bigint 2
+alive.[1] <- bigint 1
+deaths.[m + 1] <- bigint 1
 
-for i = 3 to n do
+for i = 2 to n do
     alive.[i] <- alive.[i - 1] + alive.[i - 2] - deaths.[i]
-    deaths.[i + (m - 1)] <- alive.[i - 2]
+    deaths.[i + m] <- alive.[i - 1]
 
-printfn "Alive at month %i: %A rabbits" n alive.[n]
+printfn "Alive at month %i: %A rabbit pairs" n alive.[n]
