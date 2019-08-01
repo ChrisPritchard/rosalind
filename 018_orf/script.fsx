842ed7ebeb4c1bce3ed441558002796e01d34b95
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-02 08:22:08 +1200

    slight simplifications for debugging

--- 018_orf/script.fsx
+++ 018_orf/script.fsx
@@ -1,10 +1,10 @@
 
 #load "../codons.fs"
 
-//let input = "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG"
-let input = System.IO.File.ReadAllLines "./input.txt" |> Array.tail |> String.concat ""
+let input = "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG"
+//let input = System.IO.File.ReadAllLines "./input.txt" |> Array.tail |> String.concat ""
 
-let revc dna = 
+let revc (dna: string) = 
     dna 
     |> Seq.rev
     |> Seq.map (function
@@ -13,40 +13,44 @@ let revc dna =
         | 'T' -> 'A'
         | 'G' -> 'C'
         | _ -> failwith "invalid")
+    |> Seq.toArray |> System.String
 
-let rna dna = 
-    dna |> Seq.map (function 'T' -> "U" | c -> string c) |> Seq.toArray
+let rna (dna: string) = 
+    dna |> Seq.map (function 'T' -> 'U' | c -> c) |> Seq.toArray |> System.String
 
 /// taken almost verbatim from 008_prot
-let protein rna =
+let protein (rna: string) =
     let res, finished =
         rna 
         |> Seq.chunkBySize 3 
-        |> Seq.filter (fun a -> Array.length a = 3)
+        //|> Seq.filter (fun a -> Array.length a = 3)
         |> Seq.fold (fun (acc, stopped) s -> 
             if stopped then (acc, true) 
             else 
-                let key = String.concat "" s
+                let key = System.String s
                 match Map.tryFind key Codons.table with
                 | Some v -> 
                     if v = "Stop" then (acc, true)
                     else acc + v, false
-                | None -> failwith ("invalid char " + key))
+                | None -> 
+                    let error = sprintf "invalid: %s\r\n%s" key rna
+                    failwith error)
             ("", false)
     if finished then Some res else None
 
-let openReadingFrames rna =
-    let indexes = 
+let proteins (rna: string) =
+    let openReadingFrames = 
         rna 
-        |> Array.windowed 3 |> Array.map (String.concat "") 
-        |> Array.indexed |> Array.filter (snd >> (=) "AUG") 
-        |> Array.map fst
+        |> Seq.windowed 3 |> Seq.map (Seq.toArray >> System.String) 
+        |> Seq.indexed |> Seq.filter (snd >> (=) "AUG") 
+        |> Seq.map (fst >> fun i -> rna.[i..])
+        |> Seq.toArray
 
-    indexes |> Array.map (fun i -> rna.[i..]) |> Array.choose protein
+    Array.choose protein openReadingFrames
 
 let result = 
     [|rna input; rna (revc input)|] 
-    |> Array.collect openReadingFrames 
+    |> Array.collect proteins 
     |> Array.distinct 
     |> String.concat "\r\n"
 
