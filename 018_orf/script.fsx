4ee8e0100ce0d7d3ba44fc15cc2636f95402e191
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-01 21:51:57 +1200

    partial solution - solves sample

--- 018_orf/script.fsx
+++ 018_orf/script.fsx
@@ -1,30 +1,53 @@
 
 #load "../codons.fs"
 
-let input = "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG"
-//let input = ""
+//let input = "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG"
+let input = System.IO.File.ReadAllLines "./input.txt" |> Array.tail |> String.concat ""
 
-let rna = input |> Seq.map (function 'T' -> "U" | c -> string c) |> Seq.toArray
+let revc dna = 
+    dna 
+    |> Seq.rev
+    |> Seq.map (function
+        | 'A' -> 'T'
+        | 'C' -> 'G'
+        | 'T' -> 'A'
+        | 'G' -> 'C'
+        | _ -> failwith "invalid")
 
-let indexes = 
-    rna 
-    |> Array.windowed 3 |> Array.map (String.concat "") 
-    |> Array.indexed |> Array.filter (snd >> (=) "AUG") 
-    |> Array.map fst
+let rna dna = 
+    dna |> Seq.map (function 'T' -> "U" | c -> string c) |> Seq.toArray
 
+/// taken almost verbatim from 008_prot
 let protein rna =
-    rna 
-    |> Seq.chunkBySize 3 
-    |> Seq.fold (fun (acc, stopped) s -> 
-        if stopped then (acc, true) 
-        else 
-            let v = Codons.table.[String.concat "" s]
-            if v = "Stop" then (acc, true)
-            else acc + v, false) 
-        ("", false)
-    |> fst
+    let res, finished =
+        rna 
+        |> Seq.chunkBySize 3 
+        |> Seq.filter (fun a -> Array.length a = 3)
+        |> Seq.fold (fun (acc, stopped) s -> 
+            if stopped then (acc, true) 
+            else 
+                let key = String.concat "" s
+                match Map.tryFind key Codons.table with
+                | Some v -> 
+                    if v = "Stop" then (acc, true)
+                    else acc + v, false
+                | None -> failwith ("invalid char " + key))
+            ("", false)
+    if finished then Some res else None
 
-let rnaSubs = indexes |> Array.map (fun i -> rna.[i..]) 
-let result = rnaSubs |> Array.map protein |> String.concat "\r\n"
+let openReadingFrames rna =
+    let indexes = 
+        rna 
+        |> Array.windowed 3 |> Array.map (String.concat "") 
+        |> Array.indexed |> Array.filter (snd >> (=) "AUG") 
+        |> Array.map fst
+
+    indexes |> Array.map (fun i -> rna.[i..]) |> Array.choose protein
+
+let result = 
+    [|rna input; rna (revc input)|] 
+    |> Array.collect openReadingFrames 
+    |> Array.distinct 
+    |> String.concat "\r\n"
 
 printfn "%s" result
