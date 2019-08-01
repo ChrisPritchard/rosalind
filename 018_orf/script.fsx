d2ae83b9f60ea5d9e747f85583d96998c19dd3be
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-01 15:22:40 +1200

    added rna translation

--- 018_orf/script.fsx
+++ 018_orf/script.fsx
@@ -4,6 +4,8 @@
 let input = "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG"
 //let input = ""
 
+let rna = input |> Seq.map (function 'T' -> 'U' | c -> c) |> Seq.toArray
+
 let reverseComplment = 
     input 
     |> Seq.map (function 'A' -> 'T' | 'T' -> 'A' | 'G' -> 'C' | 'C' -> 'G' | _ -> failwith "invalid") 
