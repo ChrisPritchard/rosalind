6d8fea9e274e9963af919ba4b22cfa627b48dbc5
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-01 21:09:09 +1200

    interim work on 18

--- 018_orf/script.fsx
+++ 018_orf/script.fsx
@@ -4,9 +4,27 @@
 let input = "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG"
 //let input = ""
 
-let rna = input |> Seq.map (function 'T' -> 'U' | c -> c) |> Seq.toArray
+let rna = input |> Seq.map (function 'T' -> "U" | c -> string c) |> Seq.toArray
 
-let reverseComplment = 
-    input 
-    |> Seq.map (function 'A' -> 'T' | 'T' -> 'A' | 'G' -> 'C' | 'C' -> 'G' | _ -> failwith "invalid") 
-    |> Seq.rev |> Seq.toArray
+let indexes = 
+    rna 
+    |> Array.windowed 3 |> Array.map (String.concat "") 
+    |> Array.indexed |> Array.filter (snd >> (=) "AUG") 
+    |> Array.map fst
+let substrings = indexes |> Array.map (fun i -> rna.[i..])
+
+let protein rna =
+    rna 
+    |> Seq.chunkBySize 3 
+    |> Seq.fold (fun (acc, stopped) s -> 
+        if stopped then (acc, true) 
+        else 
+            let v = Codons.table.[String.concat "" s]
+            if v = "Stop" then (acc, true)
+            else acc + v, false) 
+        ("", false)
+    |> fst
+
+let result = substrings |> Array.map protein |> String.concat "\r\n"
+
+printfn "%s" result
