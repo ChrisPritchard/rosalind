77a8bd420a94ff92d1fed5005f54d17cd5d1b110
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-05 22:06:30 +1200

    more experiments, why is this so hard

--- 024_lgis/script.fsx
+++ 024_lgis/script.fsx
@@ -7,10 +7,25 @@ open System
 let input = System.IO.File.ReadAllText "./input.txt"
 
 let raw = input.Split ("\r\n ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-let chars = Array.skip 1 raw |> Array.map int |> Array.toList
+let chars = Array.skip 1 raw |> Array.map int |> Array.indexed
 
-let rec options n rem =
-    match rem with
-    | p::rest when n > p -> rem::options n rest
-    | _::rest -> options n rest
-    | [] -> []
+let mapped predicate = 
+    chars 
+    |> Array.map (fun (i, v) -> 
+        (i, v), chars |> Array.filter (fun (oi, ov) -> oi > i && predicate v ov))
+    |> Map.ofArray
+
+// let longest mapped =
+//     let rec explore acc current =
+//         let res = Map.find current mapped
+//         if Array.isEmpty res then [|acc|]
+//         else Array.collect (fun next -> explore (next::acc) next) res
+//     chars 
+//     |> Array.collect (fun start -> explore [start] start)
+//     |> Array.maxBy List.length
+
+let test = mapped (>)
+// let long = longest test
+
+// let result = List.map string long |> String.concat " "
+printfn "%A" test
