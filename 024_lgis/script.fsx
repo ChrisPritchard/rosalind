94fe5866d1c045681522ed3ed85f14778349ed5c
Christopher <chrispritchard.nz@gmail.com>
2019-08-05 15:59:24 +1200

    ideas, experiments

--- 024_lgis/script.fsx
+++ 024_lgis/script.fsx
@@ -7,24 +7,14 @@ open System
 let input = System.IO.File.ReadAllText "./input.txt"
 
 let raw = input.Split ("\r\n ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-let chars = Array.skip 1 raw |> Array.map int |> Array.toList
+let chars = Array.skip 1 raw |> Array.map int |> Array.indexed // index, value
 
-let rec processor (rem: int list) (acc: int list list) predicate =
-    match rem with
-    | [] ->
-        let res = acc |> Seq.groupBy List.length |> Seq.maxBy fst |> snd |> Seq.last
-        res |> Seq.map string |> Seq.rev |> String.concat " "
-    | n::rem ->
-        let appended =
-            List.collect (function 
-            | p::rest when predicate p n -> [p::rest; n::p::rest]
-            | o -> [o]) acc
-        let acc = [n]::appended
-        processor rem acc predicate
+let next (index, value) p = 
+    Array.filter (fun (oi, ov) -> oi > index && p value ov) chars
 
-let inc = processor chars [] (fun p n -> n > p)
-let dec = processor chars [] (fun p n -> n < p)
-let result = inc + "\r\n" + dec
+let rec collector acc
+
+let result = ""
 
 System.IO.File.WriteAllText ("./output.txt", result)
 printfn "Result written to output.txt"
