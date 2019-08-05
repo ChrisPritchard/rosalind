8dc5061e86fa77ccd8648718113cb76fcdf22913
Christopher <chrispritchard.nz@gmail.com>
2019-08-05 15:17:49 +1200

    poorly performing solution

--- 024_lgis/script.fsx
+++ 024_lgis/script.fsx
@@ -1,9 +1,10 @@
 open System
 
-let input = """
-5
-5 1 4 2 3
-"""
+// let input = """
+// 5
+// 5 1 4 2 3
+// """
+let input = System.IO.File.ReadAllText "./input.txt"
 
 let raw = input.Split ("\r\n ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
 let chars = Array.skip 1 raw |> Array.map int |> Array.toList
@@ -11,12 +12,13 @@ let chars = Array.skip 1 raw |> Array.map int |> Array.toList
 let rec processor (rem: int list) (acc: int list list) predicate =
     match rem with
     | [] ->
-        acc |> Seq.maxBy List.length |> Seq.map string |> Seq.rev |> String.concat " "
+        let res = acc |> Seq.groupBy List.length |> Seq.maxBy fst |> snd |> Seq.last
+        res |> Seq.map string |> Seq.rev |> String.concat " "
     | n::rem ->
         let appended =
-            List.map (function 
-                | p::rest when predicate p n -> n::p::rest
-                | o -> o) acc
+            List.collect (function 
+            | p::rest when predicate p n -> [p::rest; n::p::rest]
+            | o -> [o]) acc
         let acc = [n]::appended
         processor rem acc predicate
 
