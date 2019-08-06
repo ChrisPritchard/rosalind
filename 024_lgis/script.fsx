b68c7fd0cb2e8bf66d5e55abb8f271b48b52c76d
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-06 20:17:41 +1200

    first attempted, nearly working solution to this problem

--- 024_lgis/script.fsx
+++ 024_lgis/script.fsx
@@ -6,20 +6,27 @@ open System
 // """
 let input = System.IO.File.ReadAllText "./input.txt"
 
-// trying to adapt https://www.geeksforgeeks.org/construction-of-longest-increasing-subsequence-using-dynamic-programming/
+// an adaptation of https://www.geeksforgeeks.org/construction-of-longest-increasing-subsequence-using-dynamic-programming/
 
 let raw = input.Split ("\r\n ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
 let nums = Array.skip 1 raw |> Array.map int
 
-let res = Array.create nums.Length [nums.[0]]
+let subsequence predicate =
+    let L = Array.create nums.Length []
+    L.[0] <- [nums.[0]]
 
-for i in [1..res.Length - 1] do
-    let num = nums.[i]
-    let next = 
-        match res.[i - 1] with
-        | [] -> [num]
-        | head::tail when head < num -> num::head::tail
-        | _::tail -> num::tail
-    res.[i] <- next
+    for i in [1..L.Length - 1] do
+        for j in [0..i-1] do
+            if predicate nums.[i] nums.[j] && L.[i].Length < L.[j].Length then
+                L.[i] <- L.[j]
+        L.[i] <- nums.[i]::L.[i]
 
-printfn "%A" res
+    Array.maxBy List.length L
+
+let inc = subsequence (<)
+let dec = subsequence (>)
+let concat = List.map string >> String.concat " "
+
+let result = sprintf "%s\r\n%s" (concat inc) (concat dec)
+System.IO.File.WriteAllText ("./output.txt", result)
+printfn "Result written to output.txt"
