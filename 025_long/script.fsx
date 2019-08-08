1ff0575f7494046ccb9274781cbdbdf6398df7f8
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-08 13:51:49 +1200

    implemented solver, with sample data

--- 025_long/script.fsx
+++ 025_long/script.fsx
@@ -29,4 +29,27 @@ let links =
                 |> Option.map (fun subSet -> right, subSet)))
     |> Map.ofList
 
-printfn "%A" links
+let shortest start = 
+    let next current exclude =
+        match List.except exclude links.[current] with
+        | [] -> None
+        | set -> Some (List.maxBy (snd >> fun (s: string) -> s.Length) set)
+    let rec expand soFar =
+        if List.length soFar = dna.Length then 
+            let result =
+                ("", soFar) ||> List.fold (fun acc -> 
+                    function 
+                    | token, "" -> token + acc 
+                    | token, subString -> token.Replace(subString, "") + acc)
+            Some result
+        else
+            let soFar = 
+                match soFar with
+                | [] -> Some [start, ""]
+                | (prev, _)::_ -> 
+                    next prev soFar |> Option.bind (fun n -> Some (n::soFar))
+            soFar |> Option.bind expand
+    expand []
+
+let shortestStart = dna |> List.choose shortest |> List.minBy (fun s -> s.Length)
+printfn "%s" shortestStart
