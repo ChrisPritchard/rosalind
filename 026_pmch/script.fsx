b20b801896b9bea0f4ed4ed673600bb519802e83
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-09 12:49:57 +1200

    created a naieve approach

--- 026_pmch/script.fsx
+++ 026_pmch/script.fsx
@@ -17,8 +17,13 @@ let allEdges =
         tagged 
         |> Array.choose (fun (oi, on) -> 
             if on = opposite n 
-            then Some ((i, n), (oi, on))
+            then  Some ((i, n), (oi, on))
             else None))
 
-let result = allEdges.Length
+let (_, result) = 
+    ((Set.empty, 0), allEdges)
+    ||> Seq.fold (fun (visited, count) (node1, node2) -> 
+        if visited.Contains node1 || visited.Contains node2 then visited, count
+        else (Set.add node1 visited |> Set.add node2), count + 1)
+
 printfn "Result: %i" result
