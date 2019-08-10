67dda7da8be69d4c067e5e0d2925be3b5f3a9697
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-11 09:34:40 +1200

    wrong again...

--- 026_pmch/script.fsx
+++ 026_pmch/script.fsx
@@ -7,14 +7,16 @@ open System
 // AGCUAGUCAU
 // """
 let input = """
->Rosalind_3458
-CGUUUACAAUGGUCGGAAAGCGGGCUAUGCAUCAGGUCUGAGCCUCACCACUGCAGACCU
-GGUCACUCUCAGGGGC
+>Rosalind_1305
+AGACCUCGAGACCGUAUAGAGUAUCCGUGAAAUGGAAACGGCGCCGUUCUCCUUGGGCUU
+CAGCUCUAAGUAGCUUAC
 """
 
 let dna = snd (Fasta.parse input).[0]
 let adenine = Seq.filter ((=) 'A') dna |> Seq.length
 let cytosine = (dna.Length - (adenine * 2)) / 2
 
-let result = adenine * (adenine - 1) * cytosine * (cytosine - 1)
+let fact n = if n = 0 then 1UL else List.reduce (*) [1UL..uint64 n]
+
+let result = fact adenine * fact cytosine
 printfn "Result: %i" result
