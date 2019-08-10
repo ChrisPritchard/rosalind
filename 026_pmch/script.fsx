5f9c6c83629414dde84c13c890274b3016bd1eb6
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-11 06:39:01 +1200

    another attempt. need to understand this

--- 026_pmch/script.fsx
+++ 026_pmch/script.fsx
@@ -7,14 +7,14 @@ open System
 // AGCUAGUCAU
 // """
 let input = """
->Rosalind_8725
-CCCAGCCAUUUUGCAAAACUGAACACGGGCUUUGUUCGGGCACACUCGGACUUAGAAAAA
-AGGUCUUGUUUUGA
+>Rosalind_3458
+CGUUUACAAUGGUCGGAAAGCGGGCUAUGCAUCAGGUCUGAGCCUCACCACUGCAGACCU
+GGUCACUCUCAGGGGC
 """
 
 let dna = snd (Fasta.parse input).[0]
 let adenine = Seq.filter ((=) 'A') dna |> Seq.length
 let cytosine = (dna.Length - (adenine * 2)) / 2
 
-let result = adenine * cytosine * 2
+let result = adenine * (adenine - 1) * cytosine * (cytosine - 1)
 printfn "Result: %i" result
