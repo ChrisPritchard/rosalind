2bebdbd1e3787f5ad99644a6abde2028cea8f641
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-10 17:33:16 +1200

    simplified. still missing something

--- 026_pmch/script.fsx
+++ 026_pmch/script.fsx
@@ -2,39 +2,19 @@ open System
 
 #load "../fasta.fs"
 
+// let input = """
+// >Rosalind_23
+// AGCUAGUCAU
+// """
 let input = """
->Rosalind_23
-AGCUAGUCAU
+>Rosalind_8725
+CCCAGCCAUUUUGCAAAACUGAACACGGGCUUUGUUCGGGCACACUCGGACUUAGAAAAA
+AGGUCUUGUUUUGA
 """
-//let input = System.IO.File.ReadAllText "./input.txt"
 
 let dna = snd (Fasta.parse input).[0]
-let tagged = dna |> Seq.indexed |> Seq.toArray
-let opposite = function 'A' -> 'U' | 'U' -> 'A' | 'C' -> 'G' | 'G' -> 'C' | c -> failwith ("invalid char: " + string c)
-let allEdges = 
-    tagged 
-    |> Array.collect (fun (i, n) -> 
-        tagged 
-        |> Array.choose (fun (oi, on) -> 
-            if on = opposite n 
-            then Some ((i, n), (oi, on))
-            else None))
+let adenine = Seq.filter ((=) 'A') dna |> Seq.length
+let cytosine = (dna.Length - (adenine * 2)) / 2
 
-let candidates visited =
-    allEdges 
-    |> Array.filter (fun (node1, node2) -> 
-        not (Set.contains node1 visited) && not (Set.contains node2 visited))
-
-let rec processCandidate count visited (node1, node2) =
-    let newVisited = Set.add node1 visited |> Set.add node2
-    let candidates = candidates newVisited
-    if Array.isEmpty candidates then [|count|]
-    else candidates |> Array.collect (processCandidate (count + 1) newVisited)
-
-let result = 
-    allEdges 
-    |> Array.collect (processCandidate 0 Set.empty) 
-    |> Array.sortDescending 
-    |> Array.groupBy id 
-    |> Array.head |> snd |> Array.length
-printfn "Result: %A" result
+let result = adenine * cytosine * 2
+printfn "Result: %i" result
