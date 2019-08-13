584459acc82da9d88f817f1a5f44e95ca4120b14
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-13 15:10:00 +1200

    massively overthought this. new solution much simpler.

--- 032_tree/script.fsx
+++ 032_tree/script.fsx
@@ -10,22 +10,11 @@
 // """
 let input = System.IO.File.ReadAllText "./input.txt"
 
-let parsed = input.Split ("\r\n ".ToCharArray (), System.StringSplitOptions.RemoveEmptyEntries) |> Array.map int
-let all = [1..parsed.[0]] |> Set.ofList
-let adjacencies = parsed.[1..] |> Seq.chunkBySize 2 |> Seq.map (fun a -> a.[0], a.[1]) |> Seq.toList
+let parsed = input.Split ("\r\n".ToCharArray (), System.StringSplitOptions.RemoveEmptyEntries)
+let minTotal = int parsed.[0] - 1
+let existing = parsed.[1..].Length
 
-let rec group acc unfound rem =
-    match rem with
-    | [] -> List.length acc - 1 + Set.count unfound
-    | (a, b)::rem ->
-        let acc = 
-            if Set.contains a unfound && Set.contains b unfound then Set.ofList [a;b]::acc
-            else acc |> List.map (fun set -> 
-                if set.Contains a then set.Add b 
-                elif set.Contains b then set.Add a 
-                else set)
-        let unfound = Set.remove a unfound |> Set.remove b
-        group acc unfound rem
-
-let result = group [] all adjacencies
+let result = minTotal - existing
 printfn "Result: %A" result
+
+// credit to http://garyforbis.blogspot.com/2013/07/completing-my-32nd-problem-on-rosalind.html for helping me pull my head out of my ass
