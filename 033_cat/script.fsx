166f9dc5b2ad231884d8812ab2f196c0546bd678
Christopher <chrispritchard.nz@gmail.com>
2019-08-15 21:28:57 +1200

    work on a differnt kind of solution

--- 033_cat/script.fsx
+++ 033_cat/script.fsx
@@ -1,38 +1,35 @@
 
 #load "../fasta.fs"
 
-// let input = """
-// >Rosalind_57
-// AUAU
-// """
 let input = """
->Rosalind_1001
-GAAACUAGCGAUCGUAAUUAUUUAUACGAUCGCGCGUAAACGCGUAUGCCGCGGCUCCGC
-GGAUAUUAGCUCGCCGGGCCUCGAUUCUAGUAAACGACGUUAUUCGCAGUACUGUAGCUA
-CUAGGGUAUACCCUAGCGAUUUAAAAUUGCAUAUAUCGAUGUACCGAAAGCUAUUAUCUA
-GCGAUCCUGCGUACGAUCGCAGUACCGCGCGAUGUCGACGACGUCGAUCCGGGGAUCCCG
-CGGGCGACGUCCGCGCGGCGCUAGAAUUCCGGCGCAGUAAUUUAAGCCCGCUUAAG
+>Rosalind_57
+AUAU
 """
+// let input = """
+// >Rosalind_1001
+// GAAACUAGCGAUCGUAAUUAUUUAUACGAUCGCGCGUAAACGCGUAUGCCGCGGCUCCGC
+// GGAUAUUAGCUCGCCGGGCCUCGAUUCUAGUAAACGACGUUAUUCGCAGUACUGUAGCUA
+// CUAGGGUAUACCCUAGCGAUUUAAAAUUGCAUAUAUCGAUGUACCGAAAGCUAUUAUCUA
+// GCGAUCCUGCGUACGAUCGCAGUACCGCGCGAUGUCGACGACGUCGAUCCGGGGAUCCCG
+// CGGGCGACGUCCGCGCGGCGCUAGAAUUCCGGCGCAGUAAUUUAAGCCCGCUUAAG
+// """
 
 let parsed = Fasta.parse input
 let rna = snd parsed.[0]
 
-let paired = 
-    function
-    | 'A', 'U' | 'U', 'A' | 'C', 'G' | 'G', 'C' -> true
-    | _ -> false
+let (&=&) a b = match a, b with | 'A', 'U' | 'U', 'A' | 'C', 'G' | 'G', 'C' -> true | _ -> false
 
-let folder (unmatched, count) c = 
-    match unmatched, c with
-    | [], _ -> [c], count
-    | last::rest, c when paired (last, c) -> rest, count + bigint 1
-    | _ -> c::unmatched, count
+let rec crawler low high = 
+    [low + 1..high]
+    |> List.sumBy (fun i ->
+        if rna.[low] &=& rna.[i] then
+            if high - low <= 1 then bigint 1
+            else if high - low % 2 <> 0 then bigint 0
+            else
+                crawler (low + 1) (i - 1) * crawler i high
+        else bigint 0)
 
-let _, result = Seq.fold folder ([], bigint 0) rna
+let result = crawler 0 (rna.Length - 1)
 printfn "Result: %A" result
 
-let fact n = if n = 0 then bigint 1 else List.reduce (*) [bigint 1..bigint n]
-let catalan n = fact (2 * n) / (fact (n + 1) * fact n)
-
-let test = catalan rna.Length
-printfn "Test: %A" test
+// Inspired by: https://adijo.github.io/2016/01/13/rosalind-catalan-numbers-and-rna-secondary-structures/
