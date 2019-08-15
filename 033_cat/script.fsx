81879583206e634faa0f5f80b37fa8b571bad470
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-16 10:11:40 +1200

    interim step-by-step rebuild

--- 033_cat/script.fsx
+++ 033_cat/script.fsx
@@ -17,19 +17,37 @@ AUAU
 let parsed = Fasta.parse input
 let rna = snd parsed.[0]
 
-let (&=&) a b = match a, b with | 'A', 'U' | 'U', 'A' | 'C', 'G' | 'G', 'C' -> true | _ -> false
+let paired a b = match a, b with | 'A', 'U' | 'U', 'A' | 'C', 'G' | 'G', 'C' -> true | _ -> false
 
-let rec crawler low high = 
-    [low + 1..high]
-    |> List.sumBy (fun i ->
-        if rna.[low] &=& rna.[i] then
-            if high - low <= 1 then bigint 1
-            else if high - low % 2 <> 0 then bigint 0
+// the logic is:
+// find all perfect matchings, so every nuc must be matched
+// for each nuc, find all matches where the distance between is even (so matches can run internally) and the distance after is even (so it can be matched after)
+// these finds represent possibilities, so they will be multiplied by further matches, which means sum then multiply by after
+// for both internal and external (after) of each possibility, rerun the algorithm gathering other possibilities
+// if a matching has a distance of zero (i.e. next to each other) return 1
+
+let cache = System.Collections.Generic.Dictionary<int * int, bigint>()
+
+let rec calc low high = 
+    printfn "calc %i %i" low high
+    match cache.TryGetValue ((low, high)) with
+    | true, v -> v
+    | _ ->
+        let aggregrator index = 
+            if not (paired rna.[low] rna.[index]) then 
+                printfn "%i %i not paired so 0" low index
+                bigint 0
+            else if index - low = 1 then 
+                printfn "%i %i paired and next so 1" low index
+                bigint 1
             else
-                crawler (low + 1) (i - 1) * crawler i high
-        else bigint 0)
+                printfn "multiplying for %i %i times %i %i" (low + 1) (index - 1) (index + 1) high
+                calc (low + 1) (index - 1) * calc (index + 1) high
+        let result = List.sumBy aggregrator [low + 1..2..high]
+        cache.Add((low, high), result)
+        result
 
-let result = crawler 0 (rna.Length - 1)
+let result = calc 0 (rna.Length - 1)
 printfn "Result: %A" result
 
 // Inspired by: https://adijo.github.io/2016/01/13/rosalind-catalan-numbers-and-rna-secondary-structures/
