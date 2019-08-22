14897b876ced16a923c8f4114a1535cc84c9b16f
Christopher <chrispritchard.nz@gmail.com>
2019-08-23 08:28:39 +1200

    experiments with this. i need to stop and rethink it

--- 037_kmp/script.fsx
+++ 037_kmp/script.fsx
@@ -7,17 +7,26 @@ CAGCATGGTATCACAGCAGAG
 """
 // let input = System.IO.File.ReadAllText "./input.txt"
 
-let dna = Fasta.parse input |> List.head |> snd
+let W = Fasta.parse input |> List.head |> snd |> fun s -> s.ToCharArray ()
+let T = Array.create W.Length 0
+T.[0] <- -1
+
+let mutable pos, cnd = 1, 0
+while pos < W.Length do
+    if W.[pos] = W.[cnd] then T.[pos] <- T.[cnd]
+    else
+        T.[pos] <- cnd
+        cnd <- T.[cnd]
+        while cnd >= 0 && W.[pos] <> W.[cnd] do
+            cnd <- T.[cnd]
+    pos <- pos + 1
+    cnd <- cnd + 1
+
+//T.[pos] <- cnd
 
 let result = 
-    [0..dna.Length - 1] 
-    |> List.map (fun k -> 
-        [(k - 1)..(-1)..0] 
-        |> List.tryFind (fun j -> 
-            dna.[(k-j)..k] = dna.[0..j])
-        |> Option.map (fun j -> j + 1)
-        |> Option.defaultValue 0
-        |> string)
+    T
+    |> Array.map string
     |> String.concat " "
 
 printfn "%s" result
