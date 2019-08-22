7e0c910d78b963bd78c3800de2c2d8f63f5dd7bb
Christopher <chrispritchard.nz@gmail.com>
2019-08-23 08:57:47 +1200

    experiments

--- 037_kmp/script.fsx
+++ 037_kmp/script.fsx
@@ -8,7 +8,22 @@ CAGCATGGTATCACAGCAGAG
 // let input = System.IO.File.ReadAllText "./input.txt"
 
 let W = Fasta.parse input |> List.head |> snd |> fun s -> s.ToCharArray ()
-let T = Array.create W.Length 0
+let T = Array.create W.Length -1
+T.[0] <- 0
+
+let mutable pos, len = 1, 0
+while pos < W.Length do
+    while T.[pos] = -1 do
+        if W.[len] = W.[pos] then
+            len <- len + 1
+            T.[pos] <- len
+        elif len = 0 then
+            T.[pos] <- 0
+        else
+            len <- len - 1
+    pos <- pos + 1
+
+printfn "%A" T
 
 // logic:
 // have a length index at 0
