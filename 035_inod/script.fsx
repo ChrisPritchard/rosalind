1e032c57e32b7ced052eae92287b6df636227b48
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-22 14:53:35 +1200

    solved using simple calculation

--- 035_inod/script.fsx
+++ 035_inod/script.fsx
@@ -1,11 +1,5 @@
 
-let input = 5
+let input = 5184 
 
-let rec internals connectedTo sum =
-    if connectedTo < 3 then sum
-    else
-        let nodes = int (floor (float connectedTo / 2.))
-        internals nodes (sum + nodes)
-
-let result = internals input 0
-printfn "Result: %i" result
+// this is a silly problem
+printfn "Result: %i" (input - 2)
