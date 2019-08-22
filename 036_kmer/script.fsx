5d5c9a68c047f72085bbec9af777e995df791751
Christopher <chrispritchard.nz@gmail.com>
2019-08-22 16:15:07 +1200

    solved 36 (map instead of set)

--- 036_kmer/script.fsx
+++ 036_kmer/script.fsx
@@ -1,17 +1,17 @@
 
 #load "../fasta.fs"
 
-let input = """
->Rosalind_6431
-CTTCGAAAGTTTGGGCCGAGTCTTACAGTCGGTCTTGAAGCAAAGTAACGAACTCCACGG
-CCCTGACTACCGAACCAGTTGTGAGTACTCAACTGGGTGAGAGTGCAGTCCCTATTGAGT
-TTCCGAGACTCACCGGGATTTTCGATCCAGCCTCAGTCCAGTCTTGTGGCCAACTCACCA
-AATGACGTTGGAATATCCCTGTCTAGCTCACGCAGTACTTAGTAAGAGGTCGCTGCAGCG
-GGGCAAGGAGATCGGAAAATGTGCTCTATATGCGACTAAAGCTCCTAACTTACACGTAGA
-CTTGCCCGTGTTAAAAACTCGGCTCACATGCTGTCTGCGGCTGGCTGTATACAGTATCTA
-CCTAATACCCTTCAGTTCGCCGCACAAAAGCTGGGAGTTACCGCGGAAATCACAG
-"""
-// let input = System.IO.File.ReadAllText "./input.txt"
+// let input = """
+// >Rosalind_6431
+// CTTCGAAAGTTTGGGCCGAGTCTTACAGTCGGTCTTGAAGCAAAGTAACGAACTCCACGG
+// CCCTGACTACCGAACCAGTTGTGAGTACTCAACTGGGTGAGAGTGCAGTCCCTATTGAGT
+// TTCCGAGACTCACCGGGATTTTCGATCCAGCCTCAGTCCAGTCTTGTGGCCAACTCACCA
+// AATGACGTTGGAATATCCCTGTCTAGCTCACGCAGTACTTAGTAAGAGGTCGCTGCAGCG
+// GGGCAAGGAGATCGGAAAATGTGCTCTATATGCGACTAAAGCTCCTAACTTACACGTAGA
+// CTTGCCCGTGTTAAAAACTCGGCTCACATGCTGTCTGCGGCTGGCTGTATACAGTATCTA
+// CCTAATACCCTTCAGTTCGCCGCACAAAAGCTGGGAGTTACCGCGGAAATCACAG
+// """
+let input = System.IO.File.ReadAllText "./input.txt"
 
 let dna = Fasta.parse input |> List.head |> snd
 
@@ -23,11 +23,11 @@ let rec allOptions size =
             allOptions (size - 1) 
             |> List.map (fun next -> c::next))
 
-let kMers = allOptions 4 |> List.map (fun kMer -> System.String (kMer |> List.toArray), 0) |> Set.ofList
+let kMers = allOptions 4 |> List.map (fun kMer -> System.String (kMer |> List.toArray), 0) |> Map.ofList
 let inDna = dna |> Seq.windowed 4 |> Seq.map (Array.ofSeq >> System.String) |> Seq.countBy id
-let final = Seq.foldBack Set.add inDna kMers
+let final = Seq.fold (fun map (k, v) -> Map.add k v map) kMers inDna
         
-let result = final |> Set.toList |> Seq.sortBy fst |> Seq.map (snd >> string) |> String.concat " "
+let result = final |> Map.toList |> Seq.sortBy fst |> Seq.map (snd >> string) |> String.concat " "
 
 System.IO.File.WriteAllText ("./output.txt", result)
 printfn "Result written to output.txt"
