e4212c2030807088d4db9d9f3a1cb6ab2c02a22a
Christopher <chrispritchard.nz@gmail.com>
2019-08-23 08:36:26 +1200

    thinking

--- 037_kmp/script.fsx
+++ 037_kmp/script.fsx
@@ -9,27 +9,11 @@ CAGCATGGTATCACAGCAGAG
 
 let W = Fasta.parse input |> List.head |> snd |> fun s -> s.ToCharArray ()
 let T = Array.create W.Length 0
-T.[0] <- -1
 
-let mutable pos, cnd = 1, 0
-while pos < W.Length do
-    if W.[pos] = W.[cnd] then T.[pos] <- T.[cnd]
-    else
-        T.[pos] <- cnd
-        cnd <- T.[cnd]
-        while cnd >= 0 && W.[pos] <> W.[cnd] do
-            cnd <- T.[cnd]
-    pos <- pos + 1
-    cnd <- cnd + 1
-
-//T.[pos] <- cnd
-
-let result = 
-    T
-    |> Array.map string
-    |> String.concat " "
-
-printfn "%s" result
+// logic:
+// have a length index at 0
+// if W = T at length then increase length (this means you need something to start at 0?)
+// if W <> T at length then reset to 0
 
 // System.IO.File.WriteAllText ("./output.txt", result)
 // printfn "Result written to output.txt"
