60281ae39f2dd25a2a66dc16e541b721f4fc287f
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-22 14:41:03 +1200

    initial work on script

--- 035_inod/script.fsx
+++ 035_inod/script.fsx
@@ -1,2 +1,11 @@
 
-let input = 4
+let input = 5
+
+let rec internals connectedTo sum =
+    if connectedTo < 3 then sum
+    else
+        let nodes = int (floor (float connectedTo / 2.))
+        internals nodes (sum + nodes)
+
+let result = internals input 0
+printfn "Result: %i" result
