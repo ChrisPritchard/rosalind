d6e5a489bfae48a941fc0195af2c28db2a60ad4f
Christopher <chrispritchard.nz@gmail.com>
2019-08-28 19:39:50 +1200

    solved 39

--- 039_lexv/script.fsx
+++ 039_lexv/script.fsx
@@ -1,18 +1,27 @@
 
-let input = """
-D N A
-3
-"""
 // let input = """
 // D N A
 // 3
 // """
+let input = """
+V R M N O P E Z B D
+4
+"""
 
-let alphabet = (input.Split ("\r\n".ToCharArray())).[0].Split(' ')
+let alphabet, maxLen = 
+    input.Split ("\r\n".ToCharArray(), System.StringSplitOptions.RemoveEmptyEntries)
+    |> fun a -> a.[0].Split(' '), int a.[1]
 
-let result = ""
+let rec expander prefix = 
+    [
+        for c in alphabet do
+            let res = prefix + c
+            yield res
+            if res.Length < maxLen then
+                yield! expander res
+    ]
 
-// printfn "%s" result
+let result = expander "" |> String.concat "\r\n"
 
 System.IO.File.WriteAllText ("./output.txt", result)
 printfn "Result written to output.txt"
