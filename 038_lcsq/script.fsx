319d43c460b97f862e589e5baaff0b1b5ae521db
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-28 14:43:52 +1200

    tried implementing wikipedia script

--- 038_lcsq/script.fsx
+++ 038_lcsq/script.fsx
@@ -9,16 +9,26 @@ ACACTGTGA
 """
 // let input = System.IO.File.ReadAllText "./input.txt"
 
-let contains (subSeq: char[]) (s: string) = 
-    let rec searcher subSeqIndex searchStartIndex =
-        let next = s.IndexOf(subSeq.[subSeqIndex], searchStartIndex)
-        if next = -1 then false
-        elif subSeqIndex = subSeq.Length - 1 then true
+let (s, t) = Fasta.parse input |> List.map snd |> List.toArray |> fun a -> a.[0].ToCharArray(), a.[1].ToCharArray()
+
+// An implementation of the dynamic programming solution defined here: https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
+
+let rec lcs (x: 'T[]) (y: 'T[]) =
+    if x.Length = 0 || y.Length = 0 then 0
+    else
+        let m, n = x.Length - 1, y.Length - 1
+        let p =
+            [0..min m n] |> List.tryFind (fun i -> x.[m - i] <> y.[n - i]) |> Option.defaultValue 0
+        if p > 0 then
+            (lcs x.[0..m - p] y.[0..n - p]) + p
         else
-            searcher (subSeqIndex + 1) (next + 1)
-    searcher 0 0
+            let a = lcs x.[0..m - 1] y
+            let b = lcs x y.[0..n - 1]
+            max a b
+    
+let result = lcs s t
 
-let result = ""
+printfn "%A" result
 
-System.IO.File.WriteAllText ("./output.txt", result)
-printfn "Result written to output.txt"
+// System.IO.File.WriteAllText ("./output.txt", result)
+// printfn "Result written to output.txt"
