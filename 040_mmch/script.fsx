8cc46fb05c0979d2e25a9a98a32018848e600fcd
Christopher <chrispritchard.nz@gmail.com>
2019-08-28 21:46:13 +1200

    solved 40

--- 040_mmch/script.fsx
+++ 040_mmch/script.fsx
@@ -6,14 +6,22 @@
 // AUGCUUC
 // """
 let input = """
->Rosalind_7785
-AAUGCGGUUAAGUGCGAGUUGGCAUUGCACCCCGCAAAUUAUAGUUCGAGUGACGAACUU
-GCUAGCUGUCGCGUACCGCUUCAACGACGGUGGCC
+>Rosalind_0744
+AUCUAUGAUCGAUGCCCGGUUGAGUUUCAGUGUAUGAUCGGGGCCGGUAUGUGCGGCGGU
+CCAUUAUUAAUGAGGGACGGACGUCCGCAG
 """
 
 let dna = Fasta.parse input |> List.head |> snd
 let nucs = dna |> Seq.countBy id |> Map.ofSeq
 
+let rec fact n m = 
+    if n = 0 || m = 0 then bigint 1
+    else
+        bigint n * fact (n - 1) (m - 1)
 
-let result = nucs.['A'] * nucs.['U'] * nucs.['C'] * nucs.['G']
-printfn "Result: %i" result
+let result = 
+    fact (max nucs.['A'] nucs.['U']) (min nucs.['A'] nucs.['U']) * 
+    fact (max nucs.['C'] nucs.['G']) (min nucs.['C'] nucs.['G'])
+printfn "Result: %A" result
+
+// surprising they didn't ask for mod 1 mil here.
