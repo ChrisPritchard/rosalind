2ea8671e9cebb2f5aade3f19cbe53f75076b7478
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-28 12:34:05 +1200

    experiments

--- 037_kmp/script.fsx
+++ 037_kmp/script.fsx
@@ -1,11 +1,11 @@
 
 #load "../fasta.fs"
 
-// let input = """
-// >Rosalind_87
-// CAGCATGGTATCACAGCAGAG
-// """
-let input = System.IO.File.ReadAllText "./input.txt"
+let input = """
+>Rosalind_87
+CAGCATGGTATCACAGCAGAG
+"""
+//let input = System.IO.File.ReadAllText "./input.txt"
 
 let W = Fasta.parse input |> List.head |> snd |> fun s -> s.ToCharArray ()
 let T = Array.create W.Length -1
@@ -13,18 +13,21 @@ T.[0] <- 0
 
 let mutable pos, len = 1, 0
 while pos < W.Length do
-    while T.[pos] = -1 do
-        if W.[len] = W.[pos] then
-            len <- len + 1
-            T.[pos] <- len
-        elif len = 0 then
-            T.[pos] <- 0
-        else
-            len <- T.[len - 1]
-            pos <- pos - 1
+    if W.[len] = W.[pos] then
+        len <- len + 1
+        T.[pos] <- len
+    elif len = 0 then
+        T.[pos] <- 0
+    else
+        len <- T.[len - 1]
+        pos <- pos - 1
     pos <- pos + 1
 
 let result = T |> Array.map string |> String.concat " "
 
-System.IO.File.WriteAllText ("./output.txt", result)
-printfn "Result written to output.txt"
+printfn "derived: %s" result
+printfn "tocheck: 0 0 0 1 2 0 0 0 0 0 0 1 2 1 2 3 4 5 3 0 0"
+printfn "checked: %b" (result = "0 0 0 1 2 0 0 0 0 0 0 1 2 1 2 3 4 5 3 0 0")
+
+// System.IO.File.WriteAllText ("./output.txt", result)
+// printfn "Result written to output.txt"
