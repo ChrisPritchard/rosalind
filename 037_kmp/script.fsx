a663a52995c8c0e2476b750e1f571cdb8313a6bd
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-28 12:57:30 +1200

    solved by uploading the right fucking file, this time

--- 037_kmp/script.fsx
+++ 037_kmp/script.fsx
@@ -1,33 +1,33 @@
 
 #load "../fasta.fs"
 
-let input = """
->Rosalind_87
-CAGCATGGTATCACAGCAGAG
-"""
-//let input = System.IO.File.ReadAllText "./input.txt"
+// let input = """
+// >Rosalind_87
+// CAGCATGGTATCACAGCAGAG
+// """
+let input = System.IO.File.ReadAllText "./input.txt"
 
-let W = Fasta.parse input |> List.head |> snd |> fun s -> s.ToCharArray ()
-let T = Array.create W.Length -1
-T.[0] <- 0
+let W = (Fasta.parse input |> List.head |> snd).ToCharArray ()
+let T = Array.create W.Length 0
 
 let mutable pos, len = 1, 0
 while pos < W.Length do
     if W.[len] = W.[pos] then
+        T.[pos] <- len + 1
+        pos <- pos + 1
         len <- len + 1
-        T.[pos] <- len
-    elif len = 0 then
-        T.[pos] <- 0
-    else
+    elif len > 0 then
         len <- T.[len - 1]
-        pos <- pos - 1
-    pos <- pos + 1
+    else
+        T.[pos] <- 0
+        pos <- pos + 1
+
 
 let result = T |> Array.map string |> String.concat " "
 
-printfn "derived: %s" result
-printfn "tocheck: 0 0 0 1 2 0 0 0 0 0 0 1 2 1 2 3 4 5 3 0 0"
-printfn "checked: %b" (result = "0 0 0 1 2 0 0 0 0 0 0 1 2 1 2 3 4 5 3 0 0")
+// printfn "derived: %s" result
+// printfn "tocheck: 0 0 0 1 2 0 0 0 0 0 0 1 2 1 2 3 4 5 3 0 0"
+// printfn "checked: %b" (result = "0 0 0 1 2 0 0 0 0 0 0 1 2 1 2 3 4 5 3 0 0")
 
-// System.IO.File.WriteAllText ("./output.txt", result)
-// printfn "Result written to output.txt"
+System.IO.File.WriteAllText ("./output.txt", result)
+printfn "Result written to output.txt"
