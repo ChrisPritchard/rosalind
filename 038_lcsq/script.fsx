d715305b29d6067de1da3768f8a4eeda5641e150
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-08-28 13:35:47 +1200

    started working on idea

--- 038_lcsq/script.fsx
+++ 038_lcsq/script.fsx
@@ -9,6 +9,15 @@ ACACTGTGA
 """
 // let input = System.IO.File.ReadAllText "./input.txt"
 
+let contains (subSeq: char[]) (s: string) = 
+    let rec searcher subSeqIndex searchStartIndex =
+        let next = s.IndexOf(subSeq.[subSeqIndex], searchStartIndex)
+        if next = -1 then false
+        elif subSeqIndex = subSeq.Length - 1 then true
+        else
+            searcher (subSeqIndex + 1) (next + 1)
+    searcher 0 0
+
 let result = ""
 
 System.IO.File.WriteAllText ("./output.txt", result)
