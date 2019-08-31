49c5f0aced92ca727ea624cd767cd037d209fac2
Christopher <chrispritchard.nz@gmail.com>
2019-08-31 20:02:27 +1200

    solved 45

--- 045_aspc/script.fsx
+++ 045_aspc/script.fsx
@@ -1,11 +1,12 @@
 
-let n, m = 6, 3
+//let n, m = 6, 3
+let n, m = 1876, 827
 
-let fact (n: float) = if n = 0. then 1. else List.reduce (*) [1.0..n]
-let nCk n k = 
-    let n = float n
-    let k = float k
-    fact n / (fact k * fact (n - k))
+let fact (n: bigint) = 
+    if n = bigint 0 then bigint 1 
+    else List.reduce (*) [bigint 1..n]
 
-let result = [m..n] |> List.sumBy (nCk n) |> int
-printfn "Result: %A" result
+let nCk n k = fact n / (fact k * fact (n - k))
+
+let result = [m..n] |> List.sumBy (fun k -> nCk (bigint n) (bigint k))
+printfn "Result: %A" (result % bigint 1_000_000)
