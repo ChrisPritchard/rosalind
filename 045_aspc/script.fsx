666304e08c542042b71c8ae54e9672bfa53ba9e7
Christopher <chrispritchard.nz@gmail.com>
2019-08-31 19:38:11 +1200

    initial work

--- 045_aspc/script.fsx
+++ 045_aspc/script.fsx
@@ -1,7 +1,11 @@
 
-let input = 921
+let n, k = 6, 3
 
-// this question involves the size of the 'power set', which is just 2^n
+let fact (n: float) = if n = 0. then 1. else List.reduce (*) [1.0..n]
+let nCk n k = 
+    let n = float n
+    let k = float k
+    fact n / (fact k * fact (n - k))
 
-let result = bigint.Pow (bigint 2, input) % bigint 1_000_000
+let result = nCk n k
 printfn "Result: %A" result
