18dd83959c3fc11a388b9c3b545e0c878be02526
Christopher <chris@grislygrotto.nz>
2024-04-10 08:26:58 +1200

    added reading frame support

--- src/orf.rs
+++ src/orf.rs
@@ -15,28 +15,38 @@ fn rev_complement(dna_strand: &str) -> String {
 
 pub fn solve() {
     let dna_strand1 = DATASET.split("\n").nth(1).unwrap();
-    let dna_strand2 = rev_complement(dna_strand1);
+    let dna_strand2: &str = &rev_complement(dna_strand1);
 
-    let rna_codons = |dna_strand: &str| dna_strand.chars().collect::<Vec<char>>().chunks(3).map(|s| s.iter().collect::<String>().replace("T", "U")).collect::<Vec<_>>();
+    let reading_frame_rna_codons = |dna_strand: &str, offset: usize| 
+        dna_strand.chars()
+            .skip(offset).collect::<Vec<char>>()
+            .chunks(3).map(|s| s.iter().collect::<String>().replace("T", "U"))
+            .collect::<Vec<_>>();
 
     let start_codon = "AUG";
     let codon_map = crate::util::codon_map();
 
     let mut reading = false;
     let mut current_protein = String::new();
-    for codon in rna_codons(&dna_strand2) {
-        if codon == start_codon {
-            reading = true;
-        }
-        if reading {
-            let amino_acid = codon_map.get(&codon).unwrap();
-            if amino_acid == "Stop" {
-                reading = false;
-                println!("{current_protein}");
-                current_protein.clear();
-            } else {
-                current_protein.push_str(&amino_acid);
+
+    for strand in [&dna_strand1, &dna_strand2] {
+        for frame in 0..=2 {
+            for codon in reading_frame_rna_codons(strand, frame) {
+                if codon == start_codon {
+                    reading = true;
+                }
+                if reading {
+                    let amino_acid = codon_map.get(&codon).unwrap();
+                    if amino_acid == "Stop" {
+                        reading = false;
+                        println!("{current_protein}");
+                        current_protein.clear();
+                    } else {
+                        current_protein.push_str(&amino_acid);
+                    }
+                }
             }
         }
     }
+    
 }
